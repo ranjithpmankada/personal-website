96b405ea0ad3b21e1c82d5cc507514961ade5cad
ranjith <ranjithpmankada@gmail.com>
2020-08-28 22:32:31 +0530

    Initial commits

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 const portal = ReactDOM.createPortal()
 function Loader = () => {
+    const [isLoading, setLoading] = useState(false);
+    useEffect(() => {
+        
+    });
     return (
         <div>Loading...</div>
     )
