8eaf01bb90cc4390d36766bff9d6891333b7acff
ranjith <ranjithpmankada@gmail.com>
2020-08-29 12:11:17 +0530

    Initial commits

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -28,15 +28,17 @@ function About() {
             <p>My experience, awards collaborations, and own vibes,for sure!</p>
 <p>So,</p>
 <p>I'm Ranjith - A web developer with a Computer Science Engineering major.</p>
-            <h3 className='sub-title'>Experience</h3>
-            <ul className='about_experience'>
-                {experience.map((element , index)=> (
-                <li key= {index}className='about_experience_row'>
-                    <h4 className='about_experience_title'>{element.title}</h4>
-                    <p className='about_experience_company'><a href={element.companyURL}>{element.company}</a></p>
-                    <p>{element.startDate}- {element.endDate}</p>
-                </li>))}
-            </ul>
+            <section>
+                <h3 className='sub-title'>Experience</h3>
+                <ul className='about_experience'>
+                    {experience.map((element , index)=> (
+                    <li key= {index}className='about_experience_row'>
+                        <h4 className='about_experience_title'>{element.title}</h4>
+                        <p className='about_experience_company'><a href={element.companyURL}>{element.company}</a></p>
+                        <p>{element.startDate}- {element.endDate}</p>
+                    </li>))}
+                </ul>
+            </section>
         </div>
     );
 }
