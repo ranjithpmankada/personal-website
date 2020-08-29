4d44bca02b34bb95fdaaca4fda24d9be499568e9
ranjith <ranjithpmankada@gmail.com>
2020-08-29 12:18:03 +0530

    Initial commits

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,46 +1,58 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './About.scss';
+import "./About.scss";
 function About() {
-    const experience = [
-        {
-            title: 'Front end Engineer',
-            company: 'Codinoz',
-            companyURL: 'https://codinoz.com',
-            startDate: 2018,
-            endDate: 2019
-        },
-        {
-            title: 'Web developer',
-            company: 'Coregenie Technologies',
-            companyURL: 'https://coregenie.com',
-            startDate: 2018,
-            endDate: 123232019
-        }
-    ];
-    return (
-        <div className='about'>
-            <Link to='/'>back</Link>
-            <h1 className='title'>A bit about myself</h1>
-            <p>Just about</p>
-            <p>what i do</p>
-            <p>My experience, awards collaborations, and own vibes,for sure!</p>
-<p>So,</p>
-<p>I'm Ranjith - A web developer with a Computer Science Engineering major.</p>
-            <section>
-                <h3 className='sub-title'>Experience</h3>
-                <ul className='about_experience'>
-                    {experience.map((element , index)=> (
-                    <li key= {index}className='about_experience_row'>
-                        <h4 className='about_experience_title'>{element.title}</h4>
-                        <p className='about_experience_company'><a href={element.companyURL}>{element.company}</a></p>
-                        <p>{element.startDate}- {element.endDate}</p>
-                    </li>))}
-                </ul>
-            </section>
-        </div>
-    );
+	const experience = [
+		{
+			title: "Front end Engineer",
+			company: "Codinoz",
+			companyURL: "https://codinoz.com",
+			startDate: 2018,
+			endDate: 2019,
+		},
+		{
+			title: "Web developer",
+			company: "Coregenie Technologies",
+			companyURL: "https://coregenie.com",
+			startDate: 2018,
+			endDate: 123232019,
+		},
+	];
+	return (
+		<div className='about'>
+			<Link to='/'>back</Link>
+			<h1 className='title'>A bit about myself</h1>
+			<p>Just about</p>
+			<p>what i do</p>
+			<p>My experience, awards collaborations, and own vibes,for sure!</p>
+			<p>So,</p>
+			<p>
+				I'm Ranjith - A web developer with a Computer Science
+				Engineering major.
+			</p>
+			<section>
+				<h3 className='sub-title'>Experience</h3>
+				<ul className='about_experience'>
+					{experience.map((element, index) => (
+						<li key={index} className='about_experience_row'>
+							<h4 className='about_experience_title'>
+								{element.title}
+							</h4>
+							<p className='about_experience_company'>
+								<a href={element.companyURL}>
+									{element.company}
+								</a>
+							</p>
+							<p>
+								{element.startDate}- {element.endDate}
+							</p>
+						</li>
+					))}
+				</ul>
+			</section>
+		</div>
+	);
 }
 
 export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.scss';
-import Loader from '../Loader/Loader';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import Loader from "../Loader/Loader";
 
 function Home() {
-
-    return (
-        <main className='main'>
-            <div className='container'>
-                <header className='header'>
-                    <h4 className='title_name'>Ranjith</h4>
-                    <h5 className='heading'>
-                        Creative developer &nbsp;
-                        <span className='emoji' role="img" aria-label="computer">&#128187;</span> 
-                        <br/>
-                        Moreover a JavaScript ethusiast
-                    </h5>
-                </header>
-                <nav className='nav'>
-                    <div className="nav_container">
-                        <Link className="nav_links"  to="/about">About</Link>
-                        <div className='nav_seperator'></div>
-                        <Link className="nav_links" to="/works">Works</Link>
-                        <div className='nav_seperator'></div>
-                        <Link className="nav_links" to="/contact">Contact</Link>
-                    </div>
-                </nav>
-                <footer className='footer'>
-                    <a href='https://github.com/ranjithpmankada/ ' target="_blank" rel="noopener noreferrer">Github</a>
-                    <a href='https://www.linkedin.com/in/ranjithpmankada/' target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                </footer>
-                <Loader />
-            </div>
-        </main>
-    );
+	return (
+		<main className='main'>
+			<div className='container'>
+				<header className='header'>
+					<h4 className='title_name'>Ranjith</h4>
+					<h5 className='heading'>
+						Creative developer &nbsp;
+						<span
+							className='emoji'
+							role='img'
+							aria-label='computer'
+						>
+							&#128187;
+						</span>
+						<br />
+						Moreover a JavaScript ethusiast
+					</h5>
+				</header>
+				<nav className='nav'>
+					<div className='nav_container'>
+						<Link className='nav_links' to='/about'>
+							About
+						</Link>
+						<div className='nav_seperator'></div>
+						<Link className='nav_links' to='/works'>
+							Works
+						</Link>
+						<div className='nav_seperator'></div>
+						<Link className='nav_links' to='/contact'>
+							Contact
+						</Link>
+					</div>
+				</nav>
+				<footer className='footer'>
+					<a
+						href='https://github.com/ranjithpmankada/ '
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Github
+					</a>
+					<a
+						href='https://www.linkedin.com/in/ranjithpmankada/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						LinkedIn
+					</a>
+				</footer>
+				<Loader />
+			</div>
+		</main>
+	);
 }
 
 export default Home;
